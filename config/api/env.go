bf946e6b53d80283ee6b5d22a18ab903c0127f3a
karamaru-alpha <mrnk3078@gmail.com>
2024-05-19 15:57:38 +0900

    feat: config override by env

--- config/api/env.go
+++ config/api/env.go
@@ -1,10 +1,12 @@
 package api
 
 import (
+	"log/slog"
 	"os"
 
 	"github.com/karamaru-alpha/days/pkg/derrors"
 	"github.com/karamaru-alpha/days/pkg/domain/config"
+	"github.com/karamaru-alpha/days/pkg/util/envconfig"
 )
 
 func New() (*config.APIConfig, error) {
@@ -16,6 +18,12 @@ func New() (*config.APIConfig, error) {
 	default:
 		return nil, derrors.New(derrors.Internal, "environment variable 'ENV' is invalid").SetValues(map[string]any{"ENV": env})
 	}
+
+	if err := envconfig.Load(cfg); err != nil {
+		return nil, derrors.Wrap(err, derrors.Internal, "fail to load env")
+	}
+
+	slog.Info(cfg.Port)
 
 	return cfg, nil
 }
